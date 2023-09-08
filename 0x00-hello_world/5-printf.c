7f7ae43ab61316dd594491e0eff5f14bad5e3ddd
Opeyemi Osakuade <osakuadeopeyemi@gmail.com>
2023-09-07 22:06:27 -0700

    made corrections for betty style and added comments

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - prints with proper grammar, but the outcome is a piece of art with new line
+ * Return: (0) if program runs properly
  */
 int main(void)
-{	
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
