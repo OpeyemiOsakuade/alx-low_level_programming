c8a91ec21ef30a5c084de5e29f68430546630820
Opeyemi Osakuade <osakuadeopeyemi@gmail.com>
2023-09-07 21:56:17 -0700

    made corrections for betty style and added comments

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 
 /**
  * main - prints "Programming is like building a multilingual puzzle
- * Return: Always 0 (Success)
+ * Return: 0 if program exits properly
  */
 int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
