7a807aab492a5e017d4d31bbe9e4d07e98ee7d6f
Opeyemi Osakuade <osakuadeopeyemi@gmail.com>
2023-09-15 19:05:17 -0700

    added isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,10 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * _isupper - checks for uppercase character
  *@c: int number
- * Return 1 if c is uppercase, 0 otherwise
+ *
+ *  Returns 1 if c is uppercase, 0 otherwise
  */
 int _isupper(int c)
 {
