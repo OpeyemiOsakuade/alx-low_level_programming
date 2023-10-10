4d93451c226504871f96adda997efd055fdd453b
Opeyemi Osakuade <osakuadeopeyemi@gmail.com>
2023-10-10 15:59:28 -0700

    corrected betty errors

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print alphabet - Entry point
+ * main - print alphabet
  * Return: Always 0 (Success)
  */
 
@@ -9,7 +9,7 @@ void print_alphabet(void)
 {
 	char ch;
 
-	for (ch = 'a'; ch<= 'z'; ch++)
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		_putchar(ch);
 	}
