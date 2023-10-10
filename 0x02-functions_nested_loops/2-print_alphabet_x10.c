d33c4342b0ba9cb1b1f9c100ee74dd64a1c93472
Opeyemi Osakuade <osakuadeopeyemi@gmail.com>
2023-10-10 16:43:09 -0700

    print alphabet times 10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,19 +3,18 @@
 /**
  * print_alphabet_x10 - prints the alphabet 10 times.
  *
- * Description: The function willl loop through the lowercase alphabet 
- * and print it 10 times, each is followed by a mew line
+ * Description: The function willl loop through the lowercase alphabet
+ * and print it 10 times, each is followed by a new line
  * The function utilizes the _putchar function
- * 
  * Return: Always 0
  */
 
 void print_alphabet_x10(void)
 {
-	char ch;
 	int i;
+	char ch
 
-	for (i = 10; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (ch = 'a'; ch <= 'z'; ch++)
 		{
