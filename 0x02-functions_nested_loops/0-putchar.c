0a5581eebf5f7eec74fc5648e88e43c66433ef64
Opeyemi Osakuade <osakuadeopeyemi@gmail.com>
2023-10-10 15:05:36 -0700

    program that prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+int main(void) {
+	char str[] ="_putchar\n";
+	for (int i =0; str[i]; i++){
+		.putchar(str[i]);
+	}
+	return 0;
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd>
+
+int _putchar(char c):
+
+#endif //MAIN.H
