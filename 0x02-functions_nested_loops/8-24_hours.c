5f5a7d5aa004df3caf2be3acd9c00e92ba3e0daf
Opeyemi Osakuade <osakuadeopeyemi@gmail.com>
2023-10-11 00:08:55 -0700

    added script to print every minute of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,16 +14,16 @@ void jack_bauer(void)
 	for (hour = 0; hour < 24; hour++)
 	{
 		for (minute = 0; minute < 60; minute++)
-			{
-				_putchar('0' + hour / 10);
-				_putchar('0' + hour % 10);
+		{
+			_putchar('0' + hour / 10);
+			_putchar('0' + hour % 10);
 
-				_putchar(':');
+			_putchar(':');
 
-				_putchar('0' + minute / 10);
-				_putchar('0' + minute % 10);
+			_putchar('0' + minute / 10);
+			_putchar('0' + minute % 10);
 
-				_putchar('\n');
-			}
+			_putchar('\n');
+		}
 	}
 }
