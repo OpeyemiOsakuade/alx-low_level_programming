de6237e3db2d4537c69f9e51d39d035c464935a9
Opeyemi Osakuade <osakuadeopeyemi@gmail.com>
2023-10-11 12:00:32 -0700

    added script that addds two integers

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /*
- *add - adds two integers
- *@a: first integer
- *@b: second integer
+ * add - adds two integers
+ * @a: first integer
+ * @b: second integer
+ *
  * Description: function adds the value of two integers
  * Return: the sum of a and b
  */
