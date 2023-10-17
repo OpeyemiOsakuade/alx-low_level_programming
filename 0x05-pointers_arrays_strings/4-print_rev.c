15140f93c8aa0db334d8f522cf9f34cca0e7a5cc
Opeyemi Osakuade <osakuadeopeyemi@gmail.com>
2023-10-16 22:42:42 -0700

    added function that prints string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,13 +8,14 @@
 void print_rev(char *s)
 {
 	int length = 0;
+	int i;
 
 	while (s[length])
 	{
 		length++;
 	}
 
-	for (int i = length - 1; i >= 0; i--)
+	for (i = length - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
