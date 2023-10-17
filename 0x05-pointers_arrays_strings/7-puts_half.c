3c046c885a15832e72426f133930ed4fddd17e22
Opeyemi Osakuade <osakuadeopeyemi@gmail.com>
2023-10-17 00:31:46 -0700

    solved off-by-one error, with +1

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -19,7 +19,7 @@ void puts_half(char *str)
 	if (length % 2 == 0)
 		start = length / 2;
 	else
-		start = (length - 1) / 2;
+		start = (length + 1) / 2;
 
 	for (i = start; i < length; i++)
 		_putchar(str[i]);
