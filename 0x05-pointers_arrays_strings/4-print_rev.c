6c0235ca92ac361703e9879dcbad1cfabe3422e5
Opeyemi Osakuade <osakuadeopeyemi@gmail.com>
2023-10-16 22:45:38 -0700

    added comments

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,11 +10,13 @@ void print_rev(char *s)
 	int length = 0;
 	int i;
 
+	// Calculate the length of the string
 	while (s[length])
 	{
 		length++;
 	}
 
+	//print string in reverse
 	for (i = length - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
