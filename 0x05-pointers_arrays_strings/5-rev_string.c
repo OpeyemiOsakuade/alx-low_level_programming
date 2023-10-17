94067406adf3052697a2c4a6d6c4ab3d238f8a2f
Opeyemi Osakuade <osakuadeopeyemi@gmail.com>
2023-10-16 23:20:52 -0700

    added comments that reverses a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev-string - reverses a string in-place.
+ * @s: pointer to the string to be reversed
+ */
+
+void rev_string(char *s)
+{
+	int length = 0, i;
+	char temp;
+
+	while(s[length])
+	{
+		length++;
+	}
+
+	for (i = 0; i < length / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[length - 1 - i];
+		s[length - 1 - i] = temp;
+	}
+}
