8fd2c3ae5bac8a225b9f0da7f0afa91ff9893a95
Opeyemi Osakuade <osakuadeopeyemi@gmail.com>
2023-10-20 19:51:23 -0700

    added function that concatenates at most n bytes of string src to dest

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates at most n bytes of string src to dest
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes to be concatenated
+ * Return: pointer to the resulting dest string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	char *start - dest;
+	int i = 0;
+
+	while (*dest)
+		dest++;
+
+	while (i < n && *src)
+	{
+		*dest = *src;
+		dest++;
+		src;
+		i++;
+	}
+
+	if (i < n)
+		*dest = '\0';
+
+	return (start);
+}
