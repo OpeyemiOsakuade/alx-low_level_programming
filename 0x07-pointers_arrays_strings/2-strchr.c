c0cb5e9a9701fb09c8caff2a845594c73ff73607
Opeyemi Osakuade <osakuadeopeyemi@gmail.com>
2023-10-23 15:59:19 -0700

    added fn that locates a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strchr - locates the first occurence of character c in a string 
+ * @s: the string to be searched
+ * @c: the character to search for
+ *
+ * Return: pointer to the first occurence of the character c
+ * in the string or NULL if the character is not found.
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if(*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	if (*s == c)
+	{
+		return (s);
+	}
+
+	return NULL;
+}
