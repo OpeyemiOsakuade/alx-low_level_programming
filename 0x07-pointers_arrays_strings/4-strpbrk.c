04f0d4e650bd3a8e2e9573c84087286fab28d6e6
Opeyemi Osakuade <osakuadeopeyemi@gmail.com>
2023-10-23 17:07:50 -0700

    added function that locates a substring

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes.
+ * @s: string to be searched
+ * @accept: bytes to be accepted
+ *
+ * Return: pointer to the byte in s that matches one of
+ * the bytes in accept or NULL if no such byte is found.
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	char *start_accept = accept;
+
+	while (*s)
+	{
+		accept = start_accept;
+
+		while (*accept)
+		{
+			if (*s == *accept)
+			{
+				return (s);
+			}
+			accept++;
+		}
+		s++;
+	}
+
+	return (NULL);
+}
