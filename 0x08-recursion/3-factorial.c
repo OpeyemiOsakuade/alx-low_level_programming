1a6bc95d90925d06621da64b46b24ac0de222d5c
Opeyemi Osakuade <osakuadeopeyemi@gmail.com>
2023-10-26 18:59:05 -0700

    added factorial and string_len

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * factorial - returns the factorial of a given number
+ * @n: number whose factorial will be returned
+ *
+ * Return: the factorial of n  or -1 to indicate error 
+ */
+
+int factorial(int n)
+{
+	if (n == 0)
+		return (1);
+	if (n < 0)
+		return (-1);
+	return (n * factorial(n - 1));
+}
