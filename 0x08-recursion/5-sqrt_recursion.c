36e135d20b50c107d90c665610a2215badd35bf0
Opeyemi Osakuade <osakuadeopeyemi@gmail.com>
2023-10-26 21:55:12 -0700

    added fn that calcs natural square

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * sqrt_recursion - returns the natural square root of a number
+ * @n: number whose square root to be calculated
+ *
+ * Return: the resulting square root
+ */
+
+int sqrt_helper(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (sqrt_helper(n,i + 1));
+}
+
+int _sqrt_recursion (int n)
+{
+	if (n <= 0)
+		return (-1);
+	return sqrt_helper(n, 1);
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -7,6 +7,9 @@ void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
+int _pow_recursion(int x, int y);
 int _putchar(char c);
-
+int sqrt_helper(int n, int i);
+int _sqrt_recusion(int n);
+	
 #endif /* MAIN_H */
