4fc70fb79d7a4ac6445968f866e28411092dd1bf
Opeyemi Osakuade <osakuadeopeyemi@gmail.com>
2023-11-10 12:04:41 -0800

    added cod for 2. To hell with circumstances; I create opportunities

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,31 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer
+ * @array: array to search in
+ * @size: number of elements in the array
+ * @cmp: pointer to the comparison function
+ *
+ * Return: index of the first element for which cmp does not return 0,
+ * if no match or size is negative return -1.
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL || size <= 0)
+	{
+		return -1;
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+		{
+			return i;
+		}
+	}
+
+	return -1;
+}
