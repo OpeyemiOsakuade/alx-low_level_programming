1f1d6a3011b96269353d632bbcc46da6873d2892
Opeyemi Osakuade <osakuadeopeyemi@gmail.com>
2023-11-10 11:38:25 -0800

    added stdlib header

--- 0x0F-function_pointers/1-main.c
+++ 0x0F-function_pointers/1-main.c
@@ -14,6 +14,17 @@ void print_elem(int elem)
 }
 
 /**
+ * print_elem_hex - prints an integer, in hexadecimal
+ * @elem: the integer to print
+ *
+ * Return: Nothing
+ */
+void print_elem_hex(int elem)
+{
+	printf("0x%x\n", elem);
+}
+
+/**
  * main - check the code
  *
  * Return: Always 0.
@@ -24,5 +35,6 @@ int main(void)
 	int array[5] = {0, 98, 402, 1024, 4096};
 
 	array_iterator(array, 5, &print_elem);
+	array_iterator(array, 5, &print_elem_hex);
 	return (0);
 }
