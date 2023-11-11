f9ebac9d5fd70a529530994713a10c52fec58022
Opeyemi Osakuade <osakuadeopeyemi@gmail.com>
2023-11-11 02:53:54 -0800

    satisfied betty

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,47 +1,44 @@
 #include "variadic_functions.h"
-
 /**
  * print_all - prints anything, based on the format
  * @format: A string representing the data types of the arguments.
  *
  */
-
 void print_all(const char * const format, ...)
 {
 	va_list args;
 	int i = 0;
-	char *str, format_char;
+	char *str, *sep = "";
 
 	va_start(args, format);
 
-	while (format[i] != '\0')
+	while (format != NULL && format[i] != '\0')
 	{
-		format_char = format[i];
-		if (format_char == 'c')
+		switch (format[i])
 		{
-			printf("%c", va_arg(args, int)); /* char is promoted to int */
+			case 'c':
+				printf("%c", va_arg(args, int)); /* char is promoted to int */
+				break;
+			case 'i':
+				printf("%d", va_arg(args, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(args, double)); /* float is promoted to double */
+				break;
+			case 's':
+				str = va_arg(args, char *);
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s%s", sep, str);
+				break;
 		}
-		else if (format_char == 'i')
-		{
-			printf("%d", va_arg(args, int));
-		}
-		else if (format_char == 'f')
-		{
-			printf("%f", va_arg(args, double)); /* float is promoted to double */
-		}
-		else if (format_char == 's')
-		{
-			str = va_arg(args, char *);
-			printf("%s", str ? str : "(nil)");
-			/*condition ? expression_if_true : expression_if_false. */
-		}
-
 		if (format[i + 1] != '\0')
 		{
 			printf(", ");
 		}
+		sep = ",";
+		i++;
 	}
 	va_end(args);
 	printf("\n");
 }
-
