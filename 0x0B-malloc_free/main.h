9fa5a4baa72d8cae3164d7e43c88db5e045afb01
Opeyemi Osakuade <osakuadeopeyemi@gmail.com>
2023-11-20 05:10:13 -0800

    added prototype create array to main

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,10 @@
+#ifndef MALLOC_FREE_HEADER
+#define MALLOC_FREE_HEADER
+
+/* Writes a character. */
+void _putchar(char c);
+
+/* creates an array of chars, and initializes it with a specific char. */
+char *create_array(unsigned int size, char c);
+
+#endif
