69a35551e8914f2d3d4dc3773a8f02dbbb38f4a7
Opeyemi Osakuade <osakuadeopeyemi@gmail.com>
2023-11-25 03:51:17 -0800

    edited assembly code

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,19 +1,9 @@
-section .data
-	helloString db 'Hello, Holberton', 0xA, 0X0 ; String with newline and null
-
-section .text
 	global main
 	extern printf
-
 main:
-	; Prepare arguments for printf
-	mov rdi, helloString ; First argument: address of the string
-	xor rax, rax ; Clear RAX (number of floating point arguments)
-
-	; Call printf
+	mov edi, format
+	xor eax, eax
 	call printf
-
-	; Exit program
-	mov rax, 60	; System call for exit
-	xor rdi, rdi	; Exit code 0
-	syscall
+	mov eax, 0
+	ret
+format: db 'Hello, Holberton\n',0
