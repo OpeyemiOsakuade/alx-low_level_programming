9086be63363093d7b2e0743e8ea5881a9ffa7e85
Opeyemi Osakuade <osakuadeopeyemi@gmail.com>
2023-11-28 06:13:13 -0800

    sets the value of a bit to 1 at a given index

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -18,7 +18,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 
 	/* set d bit at a specified index to 1 using OR operation */
 
-	*n |= (1ULL << index);
+	*n |= (1UL << index);
 
 	return (1);
 }
